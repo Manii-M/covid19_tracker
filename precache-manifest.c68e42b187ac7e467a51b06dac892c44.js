983f37066f04de6b307bb1e623183894d6f2056b
Manish Sahu <manii.nitc@gmail.com>
2020-04-22 15:27:53 +0530

    Updates

---
+++ precache-manifest.c68e42b187ac7e467a51b06dac892c44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204eee4d48c4c9658b5d3bb81400bde0",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "db262dd5ac586aa6f295",
+    "url": "/covid19_tracker/static/css/main.cec11cad.chunk.css"
+  },
+  {
+    "revision": "f77ebc7faa87184e581b",
+    "url": "/covid19_tracker/static/js/2.4929cfd0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_tracker/static/js/2.4929cfd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db262dd5ac586aa6f295",
+    "url": "/covid19_tracker/static/js/main.f01d2555.chunk.js"
+  },
+  {
+    "revision": "2183b9a547aec37d1c65",
+    "url": "/covid19_tracker/static/js/runtime-main.544c4f78.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19_tracker/static/media/covid19.d7265326.png"
+  }
+]);
